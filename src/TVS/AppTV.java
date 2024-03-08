f10cf1d55a4c031d3e2f1913719616ae19bef4da
JNicolasCervantesC <jncervantesc@udistrital.edu.co>
2024-03-08 07:51:40 -0500

    Tele2

--- src/TVS/AppTV.java
+++ src/TVS/AppTV.java
@@ -1,10 +1,30 @@
 package TVS;
-
+import java.util.Scanner;
 public class AppTV {
 
+	static boolean encendido()
+	{
+		boolean encendido = true;
+		return encendido;
+	}
+	
+	static boolean volumen();
+	{
+		boolena volumenM
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sacanner = new Scanner(System.in);
+		Atributostv TV1 = new Atributostv();
+		
+		boolean prender = false;
+		if(prender)
+		{
+			prender = encendido();
+		}
+		
+		
 	}
 
 }
--- src/TVS/Atributostv.java
+++ src/TVS/Atributostv.java
@@ -1,5 +1,12 @@
 package TVS;
 
 public class Atributostv {
-
+	
+	private String marca;
+	private String tamaño;
+	private boolean encendido;
+	private String[] entradas;
+	private double volumen;
+	private double canal;
+	
 }
